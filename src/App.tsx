e9b5f287183e5f2763ee807032c1b0f3300379f4
Prefect-m <mirlan.zaiyrbekov@gmail.com>
2023-06-14 22:02:58 +0600

    refactor-1

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,18 @@ import React from 'react'
 import { Player } from './components/player/Player'
 
 const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
-        border: '1px solid red',
-      }}
-    >
-      <Player src='/video/Obiwan.mp4' />
-    </div>
-  )
+	return (
+		<div
+			style={{
+				height: '100vh',
+				display: 'flex',
+				alignItems: 'center',
+				justifyContent: 'center',
+			}}
+		>
+			<Player src='/video/Obiwan.mp4' />
+		</div>
+	)
 }
 
 export default App
